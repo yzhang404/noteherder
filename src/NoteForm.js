05dec7ecbef6a82460b6b72a8c61a357fa1c126e
张雨菲 <zhangyufei@zhangyufeideair.coldspring.marian.edu>
2018-05-22 20:18:31 -0400

    Style sidebar, notelist, and noteform

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -2,9 +2,82 @@ import React from 'react'
 
 const NoteForm = () =>{
     return (
-        <div className="NoteForm">
-        NoteForm
+        <div className="NoteForm"
+        style={styles.noteForm}>
+          <div className="form-actions"
+          style={styles.formAction}>
+            <button type="button"
+            style={styles.button}>
+              <i className="fa fa-trash-o"
+              style={styles.buttonFa}></i>
+            </button>
+          </div>
+          <form style={styles.form}>
+            <p>
+              <input
+                type="text"
+                name="title"
+                placeholder="Title your note"
+                style={styles.input}
+              />
+            </p>
+            
+            <textarea name="body"
+            style={styles.textArea}></textarea>
+          </form>
         </div>
     )
+}
+
+const styles={
+    //style the noteform
+    noteForm:{
+        msFlexPositive: '1',
+        flexGrow: '1',
+        padding: '0 3rem',
+    },
+    formAction:{
+        paddingTop: '1rem',
+        marginLeft: '-2rem',
+        display: '"-ms-flexbox"',
+        display: 'flex',
+        msFlexLinePack: 'center',
+        alignContent: 'center',
+    },
+    button:{
+        border: 'none',
+        background: 'none',
+        padding: '0',
+        type: 'submit',
+        backgroundColor: '#008bf8',
+        borderRadius: '0.4rem',
+        color: '#fff',
+        fontSize: '1.4rem',
+        padding: '1rem',
+    },
+    buttonFa:{
+        color: '#999',
+        fontSize: '2rem',
+        margin: '0',
+    },
+    form:{
+        margin: '0 auto',
+        maxWidth: '80rem',
+    },
+    input:{
+        border: 'none',
+        fontSize: '200%',
+        fontFamily: '"Fauna One"',
+        color: '#008bf8',
+        fontWeight: '400',
+        width: '100%',
+        outline: 'none',
+    },
+    textArea:{
+        borderColor: '#eee',
+        width: '100%',
+        height: 'calc(100vh - 140px)',
+        fontSize: '1.3em',
+    }
 }
 export default NoteForm
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,22 +3,114 @@ import quill from './quill.svg'
 import newHover from './new-hover.png'
 import newIcon from './new.png'
 
-const Sidebar = () =>{
+
+class Sidebar extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            status: false,
+            src: newIcon
+        }
+    }
+    onButton(ev){
+        this.setState({status: false, src: newHover})
+        console.log(this.state)
+
+    }
+    offButton(ev){
+        this.setState({status: true, src:newIcon})
+        console.log(this.state)
+    }
+render(){
     return(
-        <nav className="Sidebar">
-          <div className="logo">
-            <img src={quill} alt="Noteherder"/>
+        <nav className="Sidebar"
+        style={styles.sidebar}
+        >
+          <div className="logo"
+          style = {styles.logo}
+          >
+            <img src={quill} alt="Noteherder"
+            style = {styles.logoImg}
+            />
           </div>
-          <a className="new-note" href="/notes">
-            <img src={newHover} alt="New note"/>
-            <img className="outline" src={newIcon} alt="New note"/>
+          <a className="new-note" href="/notes"
+          >
+            <img src={newHover} alt="New note"
+                style = {styles.buttonImgHover}
+             />
+            <img className="outline" src={this.state.src} alt="New note"
+                onMouseOver = {this.onButton.bind(this)}
+                onMouseOut = {this.offButton.bind(this)}
+            />
           </a>
-          <div className="SignOut">
-            <button>
-              <i className="fa fa-sign-out"></i>
+          <div className="SignOut"
+            style={styles.signOut}
+          >
+            <button
+            style = {styles.signOutButton}
+            >
+              <i className="fa fa-sign-out"
+              style = {styles.signOutButtonFa}
+              ></i>
             </button>
           </div>
         </nav>
-    )
+    );
+}
+}
+
+const mouseEvent = {
+    onMouseOver:  '<img src={newHover} alt="New note" style = {styles.buttonImgHover}/>'
+}
+const styles = {
+    sidebar:{
+        width: '6rem',
+        backgroundColor: '#f3f3f3',
+        padding: '0.5rem 0',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    logo:{
+        fontFamily: '"Fauna One"',
+        color: '#666',
+        fontSize: '3rem',
+    },
+    logoImg:{
+        width: '3rem',
+        paddingLeft: '0.4rem',
+    },
+    newNote:{
+        marginTop: '2rem',
+        position:'relative',
+        width: '4rem',
+    },
+    button:{
+        backgroundColor: 'transparent',
+        border:'0',
+        color:'#008BF8',
+        cursor: 'pointer',
+    },
+    buttonImg:{
+        position: 'absolute',
+        left:'0',
+        height: '3rem',
+        width:'3rem',
+        transition:'opacity 0.25s ease-in-out',
+    },
+    buttonImgHover:{
+        opacity:'0',
+    },
+    signOut:{
+        position: 'absolute',
+        bottom: '1rem',
+    },
+    signOutButton:{
+        outline: 'none',
+    },
+    signOutButtonFa:{
+        fontSize:'2rem',
+    }
+
 }
 export default Sidebar
