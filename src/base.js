2345b00dabf631ca9a6129355d415f9cac5e39b8
张雨菲 <zhangyufei@zhangyufeideair.coldspring.marian.edu>
2018-05-24 14:34:58 -0400

    Add firebase

---
+++ src/base.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB64VtlZtlZ6hnBJjeIsqwxbQ37ZQtoEjw",
+    authDomain: "noteherder-e45cc.firebaseapp.com",
+    databaseURL: "https://noteherder-e45cc.firebaseio.com",
+    projectId: "noteherder-e45cc",
+    storageBucket: "noteherder-e45cc.appspot.com",
+    messagingSenderId: "18520987602"
+  }
+  firebase.initializeApp(config)
