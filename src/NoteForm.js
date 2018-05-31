b9ee4d8cef212790dbccf986cae3b55465164793
张雨菲 <zhangyufei@zhangyufeideair.coldspring.marian.edu>
2018-05-31 13:44:29 -0400

    Use componentDidUpdate

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -9,20 +9,18 @@ class NoteForm extends Component {
       note: this.blankNote(),
     }
   }
-
-  componentWillReceiveProps = (newProps) => {
-    // Get the ID from the URL
-    const newId = newProps.match.params.id
-
-    // Find the note with that ID
-    const i = newProps.notes.findIndex(currentNote => currentNote.id.toString() === newId)
-    const note = newProps.notes[i] || this.blankNote()
-
-    // Update state with that note
-    if (note) {
-      this.setState({ note })
+  componentDidUpdate = () =>{
+    const newId = this.props.match.params.id ||''
+    const oldId = this.state.note.id || ''
+    if (newId !== oldId.toString){
+      const i = this.props.notes.findIndex(currentNote => currentNote)
+      const note = this.props.notes[i] || this.blankNote()
+      if (note.id !== this.state.note.id){
+      this.setState({note})
+      }
     }
   }
+
 
   blankNote = () => {
     return {
